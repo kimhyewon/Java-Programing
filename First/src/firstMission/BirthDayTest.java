9f2eeecaeeeccf0fcb635e9653c1a7b97a4a308e
GraceLilac <lovelove8926@naver.com>
2015-10-21 16:41:47 +0900

    simplify code

--- First/src/firstMission/BirthDayTest.java
+++ First/src/firstMission/BirthDayTest.java
@@ -4,20 +4,17 @@ package firstMission;
 public class BirthDayTest {
 	public static void main(String[] args) {
 		BirthDay d1 = new BirthDay(29, 2, 2000);
-		boolean result1 = d1.checkValidate();
-		printResult(result1);
+		printResult(d1.checkValidate());
 		
 		BirthDay d2 = new BirthDay(10, 7, 2014);
-		boolean result2 = d2.checkValidate();
-		printResult(result2);
+		printResult(d2.checkValidate());
 		
 		BirthDay d3 = new BirthDay(29, 2, 2100);
-		boolean result3 = d3.checkValidate();
-		printResult(result3);
+		printResult(d3.checkValidate());
 	}
 	
 	private static void printResult(boolean result) {
-		if(result == true) {
+		if(result) {
 			System.out.println("유효함");
 		}
 		else {
